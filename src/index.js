befda84cdfda03830fadc87bf1e2bc7088cba718
Sumit Sharma <sumit.sharma.gkv@gmail.com>
2024-01-09 11:19:40 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//this code depicts inline css inside javascript react course
+const customStyle = {
+  color: "red",
+  fontSize: "20 px",
+};
+
+/* the working of the render method:
+ReactDOM.render(What to show, where to show it);
+*/
+
+//inline css can also be used in the following way:
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <h1 style={customStyle}> Hello World! </h1>
+
+    <h1 style={{ color: "blue" }}>This is written in blue! </h1>
+  </div>,
+
 );
 
 // If you want to start measuring performance in your app, pass a function
